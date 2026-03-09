54de10935e33d78ecc46424f3183c557d9feb25f
WIKKI <uzkingshark@gmail.com>
2026-03-09 12:44:48 +0500

    Raise dock separator to offset 11

--- lib/src/core/widgets/common_widgets.dart
+++ lib/src/core/widgets/common_widgets.dart
@@ -152,7 +152,7 @@ class ActionDock extends StatelessWidget {
     ];
 
     return Transform.translate(
-      offset: const Offset(0, -10),
+      offset: const Offset(0, -11),
       child: Container(
         padding: EdgeInsets.zero,
         decoration: BoxDecoration(
@@ -164,7 +164,7 @@ class ActionDock extends StatelessWidget {
           ),
         ),
         child: Transform.translate(
-          offset: const Offset(0, 10),
+          offset: const Offset(0, 11),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: buttons
